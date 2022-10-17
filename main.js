cc3bcd037fd39dcf35a812e800d0f0fe70c2bf94
mukul2 <saidur.shawon@gmail.com>
2022-10-17 22:34:41 +0100

    up

--- main.js
+++ main.js
@@ -4,12 +4,23 @@ import firebase from 'firebase/app';
 import 'firebase/firestore';
 
 const firebaseConfig = {
-  // your config
+  apiKey: "AIzaSyAoTmy1Cv5MJIdJmf5uuSzax94nQ7UuyC4",
+  authDomain: "stahtohmlcrzxxz.firebaseapp.com",
+  databaseURL: "https://stahtohmlcrzxxz-default-rtdb.europe-west1.firebasedatabase.app",
+  projectId: "stahtohmlcrzxxz",
+  storageBucket: "stahtohmlcrzxxz.appspot.com",
+  messagingSenderId: "884501683260",
+  appId: "1:884501683260:web:344b6bbd3f1559eb26f739"
 };
 
 if (!firebase.apps.length) {
   firebase.initializeApp(firebaseConfig);
 }
+
+
+var room = "";
+
+var isCaller = 1;
 const firestore = firebase.firestore();
 
 const servers = {
@@ -35,10 +46,22 @@ const answerButton = document.getElementById('answerButton');
 const remoteVideo = document.getElementById('remoteVideo');
 const hangupButton = document.getElementById('hangupButton');
 
-// 1. Setup media sources
+async function startCam() {
 
-webcamButton.onclick = async () => {
-  localStream = await navigator.mediaDevices.getUserMedia({ video: true, audio: true });
+  const queryString = window.location.search;
+  const urlParams = new URLSearchParams(queryString);
+
+   room = urlParams.get('room');
+
+   isCaller = urlParams.get('isCaller');
+
+
+
+
+  localStream = await navigator.mediaDevices.getUserMedia({ video: {
+      width: { ideal: 4096 },
+      height: { ideal: 2160 }
+    } , audio: true});
   remoteStream = new MediaStream();
 
   // Push tracks from local stream to peer connection
@@ -56,19 +79,29 @@ webcamButton.onclick = async () => {
   webcamVideo.srcObject = localStream;
   remoteVideo.srcObject = remoteStream;
 
-  callButton.disabled = false;
-  answerButton.disabled = false;
-  webcamButton.disabled = true;
-};
 
+
+  if (isCaller == 1) {
+    console.log("im caller so offering")
+    callOther();
+  } else {
+    console.log("im receiver so waiting")
+    receiveCall();
+  }
+}
+
+
+// 1. Setup media sources
+startCam();
 // 2. Create an offer
-callButton.onclick = async () => {
+
+async  function callOther() {
   // Reference Firestore collections for signaling
-  const callDoc = firestore.collection('calls').doc();
+  const callDoc = firestore.collection('calls').doc(room);
   const offerCandidates = callDoc.collection('offerCandidates');
   const answerCandidates = callDoc.collection('answerCandidates');
 
-  callInput.value = callDoc.id;
+
 
   // Get candidates for caller, save to db
   pc.onicecandidate = (event) => {
@@ -105,12 +138,47 @@ callButton.onclick = async () => {
     });
   });
 
-  hangupButton.disabled = false;
-};
 
-// 3. Answer the call with the unique ID
-answerButton.onclick = async () => {
-  const callId = callInput.value;
+}
+
+var localCam = true ;
+tootoo.onclick = async () => {
+  if(localCam){
+
+    localCam = false ;
+
+    var vidTrack = localStream.getVideoTracks();
+    vidTrack.forEach(track =>
+      track.enabled = false
+     // track.stop();
+
+
+    );
+
+    tootoo.srcObject = "Turn On Camera" ;
+    //document.getElementById("tootoo").value="Turn On Camera";
+
+    // webcamVideo.srcObject = '';
+  }else{
+
+    var vidTrack = localStream.getVideoTracks();
+    vidTrack.forEach(track =>
+
+      track.enabled = true
+
+    );
+
+   // document.getElementById("tootoo").value="Turn Off Camera";
+    tootoo.srcObject = "Turn Off Camera" ;
+    localCam = true ;
+   // webcamVideo.srcObject = localStream;
+  }
+
+
+}
+
+async function receiveCall(){
+  const callId = room;
   const callDoc = firestore.collection('calls').doc(callId);
   const answerCandidates = callDoc.collection('answerCandidates');
   const offerCandidates = callDoc.collection('offerCandidates');
@@ -143,4 +211,4 @@ answerButton.onclick = async () => {
       }
     });
   });
-};
+}
